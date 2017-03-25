830d065f8cee0a5576ebcc970f51dd322438976f
ferendo <dylanfrendo.gmail.com>
2017-03-25 11:43:28 +0100

    Added classes to test the connection with Jenkins

---
+++ src/main/java/InitialJenkinsTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created by dylan on 25/03/2017.
+ */
+public class InitialJenkinsTest {
+
+    public int addFunction(int x, int y) {
+        return x + y;
+    }
+}
---
+++ src/test/java/InitialJenkinsTestTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by dylan on 25/03/2017.
+ */
+public class InitialJenkinsTestTest {
+
+}
