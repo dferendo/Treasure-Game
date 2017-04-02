33cdf37cf173c8493f358a57e9803f7d251f9cce
ferendo <dylanfrendo.gmail.com>
2017-04-02 16:51:27 +0200

    Automically Jenkins compiles and tests, test 1 - should starts #15 in Jenkins

--- src/test/java/InitialJenkinsTestTest.java
+++ src/test/java/InitialJenkinsTestTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.*;
 
 /**
  * Created by dylan on 25/03/2017.
+ * Code will be pushed 3 times to check if Jenkins pulls, compiles and tests the code
+ * automatically.
+ * Test 1 - 1 unit Test, should trigger build #15 in Jenkins.
  */
 public class InitialJenkinsTestTest {
 
@@ -22,17 +25,17 @@ public class InitialJenkinsTestTest {
         Assert.assertTrue(initialJenkinsTest.maximum(x, y) == x);
     }
 
-    @Test
-    public void minimum_firstArgumentIsMin() {
-        final int x = 10, y = 20;
-        Assert.assertTrue(initialJenkinsTest.minimum(x, y) == x);
-    }
-
-    @Test
-    public void mean_twiceMeanIsGreaterOrEqualToArguments() {
-        final int x = 10, y = 20;
-        final float twiceMean = 2 * initialJenkinsTest.mean(x, y);
-        Assert.assertTrue(twiceMean >= x);
-        Assert.assertTrue(twiceMean >= y);
-    }
+//    @Test
+//    public void minimum_firstArgumentIsMin() {
+//        final int x = 10, y = 20;
+//        Assert.assertTrue(initialJenkinsTest.minimum(x, y) == x);
+//    }
+//
+//    @Test
+//    public void mean_twiceMeanIsGreaterOrEqualToArguments() {
+//        final int x = 10, y = 20;
+//        final float twiceMean = 2 * initialJenkinsTest.mean(x, y);
+//        Assert.assertTrue(twiceMean >= x);
+//        Assert.assertTrue(twiceMean >= y);
+//    }
 }
