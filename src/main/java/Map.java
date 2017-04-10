0b6c08ab6c243ba43e42bf131ff9c59a98e6e7e4
ferendo <dylanfrendo.gmail.com>
2017-04-10 19:04:16 +0200

    Added tests for the Map class for the methods setMapSize and generate. Also added skeleton for the Map Class so that the program compiles but the tests fails

---
+++ src/main/java/Map.java
@@ -0,0 +1,12 @@
+/**
+ * @author Dylan Frendo.
+ */
+public class Map {
+
+    public boolean setMapSize(int mapSize, int numberOfPlayers) {
+        return false;
+    }
+
+    public void generate() throws Exception {
+    }
+}
---
+++ src/test/java/MapTest.java
@@ -0,0 +1,74 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Dylan Frendo.
+ */
+public class MapTest {
+
+    private Map mapInstance;
+
+    @Before
+    public void setUp() {
+        mapInstance = new Map();
+    }
+
+    @Test
+    public void setMapSize_sizeWasAlreadySet() {
+        int mapSize = 5, players = 4, mapSize2 = 8, players2 = 4;
+        if (mapInstance.setMapSize(mapSize, players)) {
+            Assert.assertTrue("Size of map was already set.", !mapInstance.setMapSize(mapSize2, players2));
+        } else {
+            fail("setMapSize was not set before hand.");
+        }
+    }
+
+    @Test
+    public void setMapSize_mapSizeIsLessThenMinimumFor2To4Players() {
+        int mapSize = 3, players = 3;
+
+        Assert.assertFalse("Map size " + mapSize + " must be greater than or equal than 5 for " + players + " players",
+                mapInstance.setMapSize(mapSize, players));
+    }
+
+    @Test
+    public void setMapSize_mapSizeIsGreaterThanMaximumSize() {
+        int mapSize = 51, players = 4;
+
+        Assert.assertFalse("Map size " + mapSize + " is greater than the maximum map size.",
+                mapInstance.setMapSize(mapSize, players));
+    }
+
+    @Test
+    public void setMapSize_mapSizeIsCorrectFor2To4Players() {
+        int mapSize = 5, players = 2;
+
+        Assert.assertTrue("Map size " + mapSize + " is correct for " + players + " players",
+                mapInstance.setMapSize(mapSize, players));
+    }
+
+    @Test
+    public void setMapSize_mapSizeIsLessThenMinimumFor5To8Players() {
+        int mapSize = 6, players = 8;
+
+        Assert.assertFalse("Map size " + mapSize + " must be greater then 8 for " + players + " players",
+                mapInstance.setMapSize(mapSize, players));
+    }
+
+    @Test
+    public void setMapSize_mapSizeIsCorrectFor5To8Players() {
+        int mapSize = 10, players = 6;
+
+        Assert.assertTrue("Map size " + mapSize + "is correct for " + players + "players.",
+                mapInstance.setMapSize(mapSize, players));
+    }
+
+    @Test(expected = Exception.class)
+    public void generate_sizeWasNotSetBeforeHand() throws Exception {
+        mapInstance.generate();
+    }
+
+}
