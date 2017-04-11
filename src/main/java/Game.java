1a9d28b45563c59ddc38f67971647e994e773b11
Miguel Dingli <dingli.miguel.15@um.edu.mt>
2017-04-11 13:12:29 +0200

    Implemented setPlayers() method.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -49,14 +49,6 @@ public class Game {
     }
 
     private void setNumPlayers() {
-        players = new Player[10];
-    }
-
-    private void setMapSize () {
-        map.setMapSize(20, 10);
-    }
-
-    private void setPlayers() {
 
         final int MIN_PLAYERS = 2, MAX_PLAYERS = 8;
         final String NUM_PLAYERS_RANGE = "(" + MIN_PLAYERS + "-" + MAX_PLAYERS + ")";
@@ -71,6 +63,40 @@ public class Game {
             } else {
                 players = new Player[numPlayers];
                 break;
+            }
+        }
+    }
+
+    private void setMapSize () {
+
+        final int MIN_MAP_SIZE = (players.length <= 4 ? 5 : 8), MAX_MAP_SIZE = 50;
+        final String MAP_SIZE_RANGE = "(" + MIN_MAP_SIZE + "-" + MAX_MAP_SIZE + ")";
+
+        int mapSize;
+        while (true) {
+            System.out.println("What will be the size of the map? " + MAP_SIZE_RANGE);
+            mapSize = getValidInt();
+
+            if (!map.setMapSize(players.length, mapSize)) {
+                System.out.println("The input value was out of the range " + MAP_SIZE_RANGE + ".");
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void setPlayers() {
+        
+        final Random rand = new Random();
+        for (int i = 0; i < players.length; i++) {
+            final Position startPos = new Position(rand.nextInt(map.getMapSize()), rand.nextInt(map.getMapSize()));
+            players[i] = new Player(i+1);
+            players[i].setPosition(startPos);
+            try {
+                map.setInitialPlayerPosition(startPos);
+            } catch(Exception e) {
+                e.printStackTrace();
+                return;
             }
         }
     }
