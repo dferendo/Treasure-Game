a617b57af2537ca7f97ed7f53044bd797a9de07e
Miguel Dingli <dingli.miguel.15@um.edu.mt>
2017-05-06 12:31:15 +0200

    Implemented addPlayer method.

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -5,7 +5,9 @@ public class Team {
 
     private List<Player> playerList = new ArrayList<Player>();
 
-    public void addPlayer(final Player player) {}
+    public void addPlayer(final Player player) {
+        playerList.add(player);
+    }
 
     public List<Player> getPlayerList() {
         return playerList;
