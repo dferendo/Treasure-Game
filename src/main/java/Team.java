6197dc8d51fa41eeab85801fd472481372a911af
Miguel Dingli <dingli.miguel.15@um.edu.mt>
2017-05-09 10:12:02 +0200

    Added team ID and test to check that it is set.

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -3,7 +3,16 @@ import java.util.List;
 
 public class Team {
 
-    private List<Player> playerList = new ArrayList<Player>();
+    private final int ID;
+    private final List<Player> playerList = new ArrayList<Player>();
+
+    public Team(int ID) {
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return ID;
+    }
 
     public void addPlayer(final Player player) {
         playerList.add(player);
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -6,22 +6,33 @@ import org.junit.Test;
 public class TeamTest {
 
     private Team team;
+    private int teamID;
 
     @Before
     public void setUp() {
-        team = new Team();
+        team = new Team(teamID);
+    }
+
+    @Test
+    public void newTeam_teamIdMatchesWithConstructorArgument() {
+        Assert.assertTrue(team.getID() == teamID);
     }
 
     @Test
     public void addPlayer_playersActuallyAddedToPlayersList() {
 
-        Player players[] = {
+        // Initialize players
+        final Player players[] = {
                 new Player(0),
                 new Player(1)
         };
+
+        // Add players to team
         for (Player p : players) {
             team.addPlayer(p);
         }
+
+        // Players in team must be equal to players in above array
         Assert.assertArrayEquals(players, team.getPlayerList().toArray());
     }
 }
