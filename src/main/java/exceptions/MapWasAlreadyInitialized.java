4445f66ce9fa0718e1b60c971439e98d14fff740
ferendo <dylanfrendo@gmail.com>
2017-05-10 10:42:24 +0200

    Added exception for map

--- src/main/java/exceptions/MapWasAlreadyInitialized.java
+++ src/main/java/exceptions/MapWasAlreadyInitialized.java
@@ -3,5 +3,9 @@ package exceptions;
 /**
  * @author Dylan Frendo.
  */
-public class MapWasAlreadyInitialized {
+public class MapWasAlreadyInitialized extends Exception {
+
+    public MapWasAlreadyInitialized() {
+        super("Map Instance already exists!");
+    }
 }
