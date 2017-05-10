db031593b406cfd417b81861d80b76a37bcbac28
ferendo <dylanfrendo@gmail.com>
2017-05-10 10:42:40 +0200

    Added creator for Map

--- src/main/java/MapCreator.java
+++ src/main/java/MapCreator.java
@@ -1,7 +1,31 @@
-package PACKAGE_NAME;
+import exceptions.MapWasAlreadyInitialized;
 
 /**
  * @author Dylan Frendo.
  */
 public class MapCreator {
+
+    public enum MAP_TYPE {
+        SAFE_MAP,
+        HAZARDOUS_MAP
+    }
+
+    public Map createMap(MAP_TYPE type) {
+        try {
+            switch (type) {
+                case SAFE_MAP:
+                    return new SafeMap();
+                case HAZARDOUS_MAP:
+                    return new HazardousMap();
+                default:
+                    return Map.getInstance();
+            }
+
+        } catch (MapWasAlreadyInitialized mapWasAlreadyInitialized) {
+            mapWasAlreadyInitialized.printStackTrace();
+        }
+        // If Map already exists, return the instance of that map.
+        return Map.getInstance();
+    }
+
 }
